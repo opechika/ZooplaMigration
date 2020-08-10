5045bd5a70a5fa9be4f09f0c10a3b0b10632fc8f
sayoafolabi <sayo_folabi@hotmail.com>
2020-08-10 20:19:49 +0100

    NO-JIRA - Adding new feature file and step definition

---
+++ src/test/java/co/uk/zoopla/features/CustomerCanSearchForPropertiesForSale.feature
@@ -0,0 +1,33 @@
+Feature: For Sale Properties
+  As a customer
+  I want the ability to search for any properties for sale
+  So that I can review the property before I buy
+
+Scenario Outline: Customer can search for any property
+  Given I navigate to zoopla homepage
+  When I enter "<Location>" into the search field
+  And I select "<MinPrice>" from Min price dropdown
+  And I select "<MaxPrice>" from Max price dropdown
+  And I select "<Property>" from Property type dropdown
+  And I select "<Bedroom>" from Bedrooms dropdown
+  And I click on Search button
+  Then search result page is displayed with relevant results
+
+  Examples:
+  |Location|MinPrice|MaxPrice|Property|Bedroom|
+  |Manchester| £140,000|£250,000|Houses|3+    |
+  |OL9 8LD   | £120,000|£180,000|Flats |2+    |
+  |Victoria Station| £200,000|£400,000|Houses|4+|
+  |OL9    | £120,000|£180,000|Flats |2+    |
+
+  Scenario: Customer cannot search for empty location
+    Given I navigate to zoopla homepage
+    When I enter "" into the search field
+    And I click on Search button
+    Then an error message is displayed on search field
+
+  Scenario: Customer gets error page for invalid location
+    Given I navigate to zoopla homepage
+    When I enter "wwwwwwwwww" into the search field
+    And I click on Search button
+    Then an error page is displayed
---
+++ src/test/java/co/uk/zoopla/stepDefinitions/ForSaleSearchSteps.java
@@ -0,0 +1,60 @@
+package co.uk.zoopla.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ForSaleSearchSteps {
+
+    @Given("I navigate to zoopla homepage")
+    public void i_navigate_to_zoopla_homepage() {
+
+    }
+
+    @When("I enter {string} into the search field")
+    public void i_enter_into_the_search_field(String string) {
+
+    }
+
+    @When("I select {string} from Min price dropdown")
+    public void i_select_from_min_price_dropdown(String string) {
+
+    }
+
+    @When("I select {string} from Max price dropdown")
+    public void i_select_from_max_price_dropdown(String string) {
+
+    }
+
+    @When("I select {string} from Property type dropdown")
+    public void i_select_from_property_type_dropdown(String string) {
+
+    }
+
+    @When("I select {string} from Bedrooms dropdown")
+    public void i_select_from_bedrooms_dropdown(String string) {
+
+    }
+
+    @When("I click on Search button")
+    public void i_click_on_search_button() {
+
+    }
+
+    @Then("search result page is displayed with relevant results")
+    public void search_result_page_is_displayed_with_relevant_results() {
+
+    }
+
+    @Then("an error message is displayed on search field")
+    public void an_error_message_is_displayed_on_search_field() {
+
+    }
+
+    @Then("an error page is displayed")
+    public void an_error_page_is_displayed() {
+
+    }
+
+
+}
